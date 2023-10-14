0b9c962e4242ae0689261f8b4a24483a96441507
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2023-10-14 20:08:07 -0300

    inicio do projeto no backend

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+require("dotenv").config()
+
+const express = require("express")
+const path = require("path")
+const cors = require("cors")
+
+const port = process.env.PORT;
+
+const app = express()
+
+//config JSON and form data response
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended:false}))
+
+
+// routes
+
+const router = require("./routes/Router")
+
+app.use(router)
+
+app.listen(port, () =>{
+    console.log(`App rodando na porta ${port}`)
+})
---
+++ backend/routes/Router.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const router = express()
+
+// test route
+
+router.get("/", (req,res) =>{
+    res.send("API Working!")
+})
+
+
+module.exports = router
