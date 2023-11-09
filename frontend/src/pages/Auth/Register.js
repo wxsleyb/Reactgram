ee78bcb59af863c111a5302c48d08d393bbb2be3
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2023-11-08 22:04:12 -0300

    css global e do registro

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,8 +1,32 @@
 import "./Auth.css"
+
+// components
+import {Link} from 'react-router-dom'
+
+//Hooks
+import {useState, useEffect} from 'react'
 const Register = () => {
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+
+  }
+
   return (
-    <div>
-      Register
+    <div id="register">
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a senha" />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já tem conta? <Link to="/login">Clique aqui</Link>
+      </p>
+
     </div>
   )
 }
