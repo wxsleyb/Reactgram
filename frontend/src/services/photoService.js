2449f990ab8269a2481cc3e859830ffd3d836577
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2023-12-03 18:07:44 -0300

    service e slice pegar fotos usuario

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -18,8 +18,29 @@ const publishPhoto = async(data,token) =>{
 }
 
 
+// pegar as fotos do usuario
+
+const getUserPhotos = async(id, token) =>{
+    
+    const config = requestConfig("GET", null, token)
+
+    try {
+        const res = await fetch(api + "/photos/user/"+ id, config)
+            .then((res)=> res.json())
+            .catch((err) => err)
+            return res
+
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
 const photoService = {
-    publishPhoto
+    publishPhoto,
+    getUserPhotos,
 }
 
 export default photoService;
--- frontend/src/slices/photoSlice.js
+++ frontend/src/slices/photoSlice.js
@@ -30,6 +30,20 @@ export const publishPhoto = createAsyncThunk(
     }
 )
 
+//pegar as fotos do usuario
+
+export const getUserPhotos =  createAsyncThunk(
+    "photo/userphotos",
+    async(id, thunkAPI)=>{
+        
+        const token = thunkAPI.getState().auth.user.token
+
+        const data = await photoService.getUserPhotos(id,token)
+
+        return data
+    }
+)
+
 
 
 export const photoSlice = createSlice({
@@ -58,6 +72,16 @@ export const photoSlice = createSlice({
             state.loading = false;
             state.error = action.payload;
             state.photo = {};
+          })
+          .addCase(getUserPhotos.pending, (state) => {
+            state.loading = true;
+            state.error = false;
+          })
+          .addCase(getUserPhotos.fulfilled, (state, action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            state.photos = action.payload;
           })
     }
 })
